dde957540e63e132a65d018d80158473d06c3afa
Melvin Alvarez <alvarezmelvin@me.com>
2017-09-18 15:22:00 +0000

    sidebar buttons

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,28 @@ $('.grid').isotope({
     itemSelector: '.grid-item',
 });
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("sidebar-backtotop").style.visibility = "visible";
+    } else {
+        document.getElementById("sidebar-backtotop").style.visibility = "hidden";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+//function topFunction() {
+//    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+//    document.documentElement.scrollTop = 0; // For IE and Firefox
+//}
+
+$('#sidebar-backtotop').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+ });
+
 
 // Cursor Interactions
 var box=$(".box");
